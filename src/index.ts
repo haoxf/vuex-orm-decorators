a2f0c75da3c007b099fef5a5f1fdc48509079659
haoxf <dp282074009@126.com>
2020-09-09 16:16:35 +0800

    fix: add DateField export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import { Attribute, Model } from '@vuex-orm/core';
-import Mutator from '@vuex-orm/core/lib/attributes/contracts/Mutator';
-
-export { OrmModel } from './model';
-export { ORMDatabase } from './database';
+export { OrmModel } from './model'
+export { ORMDatabase } from './database'
 
 export {
-    AttrField, BelongsToField, BelongsToManyField, BooleanField, Field, HasManyByField,
-    HasManyField, HasManyThroughField, HasOneField, IncrementField, MorphManyField,
-    MorphOneField, MorphToField, MorphToManyField, MorphedByManyField, NumberField,
-    PrimaryKey, StringField
-} from './attributes';
+  AttrField,
+  BelongsToField,
+  BelongsToManyField,
+  BooleanField,
+  Field,
+  HasManyByField,
+  HasManyField,
+  HasManyThroughField,
+  HasOneField,
+  IncrementField,
+  MorphManyField,
+  MorphOneField,
+  MorphToField,
+  MorphToManyField,
+  MorphedByManyField,
+  NumberField,
+  PrimaryKey,
+  StringField,
+  DateField,
+} from './attributes'
